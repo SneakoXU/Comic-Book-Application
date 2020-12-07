75a6b55e246249743fbb3d7fb57cc3fce1b926e1
Terrell Singletary <terrellsingle359@gmail.com>
2020-12-07 11:58:24 -0500

    Home.vue update

--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <h1>Home</h1>
+    <h1 id="header">Home</h1>
     <p>You must be authenticated to see this</p>
   </div>
 </template>
@@ -10,3 +10,14 @@ export default {
   name: "home"
 };
 </script>
+
+<style>
+  div.home {
+    background-image: url("../../assets/Images/blue backgd.jpg");
+    background-size: 100%;
+    background-repeat: no-repeat;    
+  };
+  #header {
+    background-image: "../../assets/Images/nav bar.png";
+  }
+</style>
