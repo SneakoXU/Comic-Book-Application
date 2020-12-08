3a584e3fe1fe911494f5f6a8c81497020c8639a5
Terrell Singletary <terrellsingle359@gmail.com>
2020-12-08 13:31:12 -0500

    CollectionCard and Home connectivity

---
+++ vue/src/components/CollectionCard.vue
@@ -0,0 +1,81 @@
+<template>
+<div class="card">
+  <h1 class="title">{{collection.title}}</h1>
+  <h3 class="owner">By: {{collection.owner}}</h3>
+  <div class="container">
+    <img id="face" src="../../assets/Images/Captain_America_Comics_Vol_1_2.jpg" alt="">
+    <div class="view">
+      Click to view more
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'collection-card',  
+  data() {
+    return {
+      collection:{
+        title: 'Test',
+        owner: 'Me',
+        dateCreated: '',
+        comics: []
+      }
+    };
+  },
+  methods: {
+
+    getAllComics() {
+      
+    }
+    
+  }  
+
+}
+</script>
+
+<style>
+  div.card{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    transition: .5s ease;
+    margin: 30px;
+  }
+
+  #face {
+    transition: .5s ease;
+    opacity: 1;
+  }
+
+  .view {
+    transition: .5s ease;
+    opacity: 0;
+    position: absolute;
+    top:50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    -ms-transform: translate(-50%,-50%);
+    text-align: center;
+  }
+
+  .title, .owner, .view {
+    text-shadow: 2px 2px #000;
+    color: white;
+  }
+
+   div.container:hover #face{
+    opacity: 0.5;
+  }
+
+   div.container:hover .view{
+    opacity: 1;
+  }
+
+  #face {
+    height: 350px;
+    width: 275px;
+  }
+
+</style>
