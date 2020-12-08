deaf2ce191f62f6338e804c8230ac5b07173cd71
Terrell Singletary <terrellsingle359@gmail.com>
2020-12-08 08:11:47 -0500

    Added collection.vue

---
+++ vue/src/components/Collection.vue
@@ -0,0 +1,19 @@
+<template>
+<div class="collection">
+  <h3 class="title">{{collection.title}}</h3>
+  <h2>By: {{collection.owner}}</h2>
+  <img src="" alt="">
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+    .collection{
+
+    }
+</style>
