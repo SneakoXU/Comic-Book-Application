4c7caa1dc141d3ac9cc2d0ad8f6b735476cc76b8
Niko Suntay <suntay.3@gmail.com>
2020-12-08 15:46:04 -0500

    updated header style

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -2,10 +2,12 @@
   <div id="app">
     <div id="nav">
       <img class="logo" src="../assets/Images/BTC icon.png" alt="BT Comic Logo">
+      <div class="itemBox">
       <router-link class="navItem" v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
       <router-link class="navItem" v-bind:to="{ name: 'login' }">Login</router-link>&nbsp;|&nbsp;
       <router-link class="navItem" v-bind:to="{ name: 'register' }">Register</router-link>&nbsp;|&nbsp;
       <router-link class="navItem" v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
+      </div>
     </div>
     <router-view />
   </div>
@@ -37,10 +39,19 @@
     
   }
 
-  #navItem{
+  .itemBox{
     display: flex;
-    align-content: flex-end;
+    width: 100%;
+    justify-content: flex-end;
   }
+
+  /* .navItem{
+    display: flex;
+    justify-content: space-evenly;
+    width: 100%;
+    padding: 2px 2px 2px 2px;
+    margin: 2px 2px 2px 2px;
+  } */
 
   img{
     height: 70px;
