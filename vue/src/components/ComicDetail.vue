28b77c106f51b469227fb670b477a31b4065ee0d
Antoine <antoine_t_fulton@yahoo.com>
2020-12-09 11:46:13 -0500

    Started Comic detail

--- vue/src/components/ComicDetail.vue
+++ vue/src/components/ComicDetail.vue
@@ -1,10 +1,29 @@
 <template>
+<div class="comic-info">
+    <h2 class="title">{{comic.title}}</h2>
+    <h3 class="author">{{comic.author}}</h3>
+    <h4 class="description">{{comic.description}}</h4>
+    <div class="container">
+        <img id="comic-page" src="../../assets/Images/Captain_America_Comics_Vol_1_2.jpg" alt="">
   
 </template>
 
 <script>
 export default {
+    name: 'comic-detail',
+    data() {
+        return {
+            comic:{
+                title: '',
+                author: '',
+                description: '',
 
+            }
+        }
+    },
+    methods: {
+
+    }
 }
 </script>
 
