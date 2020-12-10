13f6469b62e2e4e69ee5127e4c83b07e6883b19e
Niko Suntay <suntay.3@gmail.com>
2020-12-10 14:17:54 -0500

    Search.vue

---
+++ vue/src/views/Search.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+      <search-comic></search-comic>
+  </div>
+</template>
+
+<script>
+import SearchComic from '../components/SearchComic.vue';
+export default {
+    components: {
+        SearchComic,
+    },
+    name: 'search'
+
+}
+</script>
+
+<style>
+
+</style>
