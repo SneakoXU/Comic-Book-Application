9467f504175d4fa851840501b35710e5382ee673
Niko Suntay <suntay.3@gmail.com>
2020-12-11 14:12:13 -0500

    addcollection update

--- vue/src/services/CollectionService.js
+++ vue/src/services/CollectionService.js
@@ -23,7 +23,7 @@ export default{
     },
 
     addCollection(collection){
-        return axios.post('/collections/create', collection)
+        return axios.post('/collections/createraw', collection)
     },
 
     getCollectionThumbnail(id) {
